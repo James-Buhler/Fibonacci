60b4f6680fc348f43f813c776de6a1031aebdbbd
James-Buhler <129095464+James-Buhler@users.noreply.github.com>
2023-09-26 22:31:04 -0500

    added counting

--- src/fib/driver.java
+++ src/fib/driver.java
@@ -1,12 +1,14 @@
 package fib;
 
 public class driver {
-
+	public static int count = 0;
 	public static void main(String[] args) {
 		System.out.println(getFibanacci(5));
+		System.out.println(count);
 	}
 
 	public static int getFibanacci(int index) {
+		count++;
 		int fib1 = 0;
 		int fib2 = 1;
 		for(int i = 0; i < index; i++) {
